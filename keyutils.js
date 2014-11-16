080b9c43d87bf035d420926bd8ccdfa4b65e55b8
zhengpj3 <zpj090724@163.com>
2014-11-16 15:21:44 +0800

    完成按键名称匹配

--- keyutils.js
+++ keyutils.js
@@ -22,6 +22,39 @@
 	if(typeof window.KeyUtils != "undefined") {
 	    var _KeyUtils = window.KeyUtils;
 	}
+	//1. 传入key的code，返回确定的字符
+	var getKeyName =function(code){
+		var key_names = {
+			8: 'BACKSPACE',9: 'TAB',13: 'ENTER',16: 'SHIFT',17: 'CTRL',18: 'ALT',19: 'PAUSE',
+			20: 'CAPS_LOCK',27: 'ESCAPE',32: 'SPACE',33: 'PAGE_UP',34: 'PAGE_DOWN',35: 'END',
+			36: 'HOME',37: 'LEFT',38: 'UP',39: 'RIGHT',40: 'DOWN',45: 'INSERT',46: 'DELETE',
+			144: 'NUM_LOCK',145: 'SCROLL_LOCK',222: "'"
+		};
+		//字母和数字键的键码值
+		var num1_name={48:'0',49:'1', 50:'2',51:'3',52:'4',53:'5',54:'6',55:'7',56:'8',57:'9'};
+		var letter_name={65:'A',66:'B',67:'C',68:'D',69:'E',70:'F',71:'G',72:'H', 73:'I',74:'J',
+	        	  		 75:'K',76:'L',77:'M',78:'N',79:'O',80:'P',81:'Q',82:'R',83:'S',84:'T',
+	        	  		 85:'U',86:'V',87:'W',88:'X',89:'Y',90:'Z'};
+	    // 数字键盘上的键的键码值
+	    var num_name={96:'NUM0',97:'NUM1',98:'NUM2',99:'NUM3',100:'NUM4',101:'NUM5',102:'NUM6',		 	 	 	 
+					  103:'NUM7',104:'NUM8',105:'NUM9',106:'NUM*',107:'NUM+',108:'NUMENTER',	
+					  109:'NUM-',110:'NUM.',111:'NUM/'};
+		//功能键键码值
+		var fn_name={112:'F1',113:'F2',114:'F3',115:'F4',116:'F5',117:'F6',118:'F7',	
+					 119:'F8',120:'F9',121:'F10',122:'F11',123:'F12'};
+		//控制键键码值
+		var cont_name={8:'BACKSPACE',9:'TAB',12:'CLEAR',13:'ENTER',16:'SHIFT',17:'CTRL',		
+					   18:'ALT',20:'CAPE_LOCK',27:'ESC',32:'SPACEBAR',33:'PAGE_UP',34:'PAGE_DOWN',		
+					   35:'END',36:'HOME',37:'LEFT',38:'UP',39:'RIGHT',40:'DOWN',45:'INSERT',	
+					   46:'DELETE',144	:'NUM_LOCK',	
+					   186:';',187:'=',188:',',189:'-',190:'.',191:'/',192:'`',219:'[',220:"\\",	
+					   221:']',222:"'"};
+		if(code>47&&code<58){return num1_name[code];}
+		else if(code>64&&code<91){return letter_name[code];}
+		else if(code>95&&code<112){return num_name[code];}
+		else if(code>111&&code<124){return fn_name[code];}
+		else {return cont_name[code]};
+	};
 	var event_map      =new Array();
 	var keyArr=[];//存储按键
 	var current_keys = {};
@@ -30,26 +63,7 @@
 	//读取自定义属性data-hotkey
 	var  hotkeyArr = new Array;
 	//$('[data-hotkey]').each(function(i, element) {hotkeyArr.push(element);});
-	var key_names = {
-		8: 'BACKSPACE',9: 'TAB',13: 'ENTER',16: 'SHIFT',17: 'CTRL',18: 'ALT',19: 'PAUSE',
-		20: 'CAPS_LOCK',27: 'ESCAPE',32: 'SPACE',33: 'PAGE_UP',34: 'PAGE_DOWN',35: 'END',
-		36: 'HOME',37: 'LEFT',38: 'UP',39: 'RIGHT',40: 'DOWN',45: 'INSERT',46: 'DELETE',
-		144: 'NUM_LOCK',145: 'SCROLL_LOCK',222: "'"
-	};
-	//F1~F12
-	function fn_name(code) {
-	    return (code >= 112 && code <= 123)?'F' + (code - 111):false;
-	};
-	//数字键盘按键
-	function num_name(code) {
-	    if(code >= 96 && code < 106) return 'Num' + (code - 96);
-	    switch(code) {
-	        case 106: return 'Num*';
-	        case 111: return 'Num/';
-	        case 110: return 'Num.';
-	        default: return false;
-	    }
-	};
+	
 	//判断数组当中是否存在某元素
 	var isInArray=function(str,arr){
 	  var i = arr.length; 
